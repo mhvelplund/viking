c35da2d5ee9899d510f80a960a76edb1a39d2b91
Mads Hvelplund <mhvelplund@users.noreply.github.com>
2021-08-01 23:01:21 +0200

    Finished scale and rotate method

--- src/FightScene.lua
+++ src/FightScene.lua
@@ -4,35 +4,9 @@ local FightScene = Scene:extend()
 local viking1, shield1, sword1
 local viking2, shield2, sword2
 
--- Draw the drawable in mirrored from on a canvas and return that.
-local function mirrorDrawable(drawable)
-    local w,h = drawable:getDimensions()
-    local canvas = love.graphics.newCanvas(w,h)
-    love.graphics.setCanvas(canvas)
-    love.graphics.clear()
-    love.graphics.draw( drawable, w,0,0,-1,1)
-    love.graphics.setCanvas()
-    return canvas
-end
-
--- Rotate a drawable a number of degrees counterclockwise, and return it on a canvas.
-local function rotateDrawable(drawable,rotation)
-    local w,h = drawable:getDimensions()
-    local canvas = love.graphics.newCanvas(w,h)
-    love.graphics.setCanvas(canvas)
-    love.graphics.clear()
-    love.graphics.draw( drawable, w/2,w/2,(math.pi/180)*rotation,1,1,w/2,w/2)
-    love.graphics.setCanvas()
-    return canvas
-end
-
 function FightScene:constructor()
-    -- Load the first viking image
     viking1 = love.graphics.newImage( "img/viking.png" )
-
-    -- Load a mirrored version onto a canvas
-    viking2 = mirrorDrawable(love.graphics.newImage( "img/mongol.png" ))
-
+    viking2 = love.graphics.newImage( "img/mongol.png" )
     shield1 = love.graphics.newImage( "img/shield.png" )
     shield2 = love.graphics.newImage( "img/shield.png" )
     sword1 = love.graphics.newImage( "img/sword.png" )
@@ -45,18 +19,51 @@ function FightScene:update(dt)
     end
 end
 
+local function scaleAndRotate(drawable, options)
+    options = options or {}
+    local dw,dh = drawable:getDimensions()
+    local scale = options.scale or 1
+    local w = options.width or dw*scale
+    local h = options.height or dh*scale
+    local mirror = options.mirror and -1 or 1
+    local rotation = options.rotation or 0
+    local wscale, hscale = scale,scale
+
+    if w ~= dw and not wscale then
+        wscale = w/dw
+    end
+
+    if h ~= dh and not hscale then
+        hscale = h/dh
+    end
+
+    wscale = wscale * mirror
+
+    local canvas = love.graphics.newCanvas(w,h)
+    local original = love.graphics.getCanvas() -- this is needed because the push library is already using a canvas
+    love.graphics.setCanvas(canvas)
+    love.graphics.draw( drawable, w/2, h/2, (math.pi/180)*rotation, wscale, hscale, dw/2, dh/2)
+    love.graphics.setCanvas(original)
+    return canvas
+end
+
 function FightScene:render()
     -- Draw second combatant
-    local v1w,v1h = viking1:getDimensions()
-    local scale1 = (VIRTUAL_HEIGHT*0.8)/v1h
-    local v1x, v1y = (VIRTUAL_WIDTH/4)-(v1w/2)*scale1, (VIRTUAL_HEIGHT/2)-(v1h/2)*scale1
-    love.graphics.draw( viking1, v1x,v1y,0,scale1,scale1)
+    local v1 = scaleAndRotate(viking1, {
+        scale = (VIRTUAL_HEIGHT*0.8)/({viking1:getDimensions()})[2]
+    })
+    local v1w,v1h = v1:getDimensions()
+    local v1x, v1y = (VIRTUAL_WIDTH/4)-(v1w/2), (VIRTUAL_HEIGHT/2)-(v1h/2)
+    love.graphics.draw( v1, v1x,v1y)
 
-    -- Draw second combatant
-    local v2w,v2h = viking2:getDimensions()
-    local scale2 = (VIRTUAL_HEIGHT*0.8)/v2h
-    local v2x, v2y = (VIRTUAL_WIDTH/4)*3-(v2w/2)*scale2, (VIRTUAL_HEIGHT/2)-(v2h/2)*scale2
-    love.graphics.draw( viking2, v2x,v2y,0,scale2,scale2)
+    -- -- Draw second combatant
+    local v2 = scaleAndRotate(viking2, {
+        scale = (VIRTUAL_HEIGHT*0.8)/({viking2:getDimensions()})[2],
+        mirror = true
+    })
+    local v2w,v2h = v2:getDimensions()
+    local v2x, v2y = (VIRTUAL_WIDTH/4)*3-(v2w/2), (VIRTUAL_HEIGHT/2)-(v2h/2)
+    love.graphics.draw( v2, v2x,v2y)
 end
 
 return FightScene
